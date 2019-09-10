56c5774556e905258897fd051e6d91c66c1b81fc
Markus Birth <mbirth@gmail.com>
2019-09-10 16:51:56 +0200

    Support multiple SKUs in one call.

--- get_updates.py
+++ get_updates.py
@@ -9,23 +9,23 @@ from grmn import updateserver, devices
 import sys
 
 if len(sys.argv) < 2:
-    print("Syntax: {} DEVICESKU".format(sys.argv[0]))
+    print("Syntax: {} DEVICESKU1 [DEVICESKU2..n]".format(sys.argv[0]))
     print("Examples:")
     print("  {} 3196           - Query update info for 006-B3196-00".format(sys.argv[0]))
     print("  {} 006-B3196-00   - Query update info for given SKU".format(sys.argv[0]))
     sys.exit(1)
 
-DEVICE = sys.argv[1]
+device_skus = sys.argv[1:]
 
 us = updateserver.UpdateServer()
 
-device_sku = DEVICE
-if len(device_sku) <= 4:
-    device_sku = "006-B{:>04}-00".format(device_sku)
+for i, sku in enumerate(device_skus):
+    if len(sku) <= 4:
+        device_skus[i] = "006-B{:>04}-00".format(sku)
 
-hwid = int(device_sku[5:9])
-device_name = devices.DEVICES.get(hwid, "Unknown device")
+primary_hwid = int(device_skus[0][5:9])
+device_name = devices.DEVICES.get(primary_hwid, "Unknown device")
 
 print("Device: {}".format(device_name))
 
-us.query_updates([device_sku])
+us.query_updates(device_skus)
