98263e6af2c28b33ed3960e462b9b2cb1bc12590
Markus Birth <mbirth@gmail.com>
2019-10-11 16:30:01 +0200

    Updated .gitignore. Added --verbose parameter to gcdstruct.py.

--- gcdstruct.py
+++ gcdstruct.py
@@ -8,11 +8,23 @@ Prints out the structure of the given GCD file.
 from grmn import Gcd, ChkSum
 import sys
 
-FILE = sys.argv[1]
+if len(sys.argv) <= 2:
+    VERBOSE = False
+    FILE = sys.argv[1]
+elif sys.argv[1] == "--verbose":
+    VERBOSE = True
+    FILE = sys.argv[2]
+elif sys.argv[2] == "--verbose":
+    VERBOSE = True
+    FILE = sys.argv[1]
 
 print("Opening {}".format(FILE))
 
 gcd = Gcd(FILE)
 
-gcd.print_struct()
+if VERBOSE:
+    gcd.print_struct_full()
+else:
+    gcd.print_struct()
+
 gcd.validate(True)
